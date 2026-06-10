70b1d8e9d617f8104240df57e25b347c61faf149
Armand <Armand>
2026-06-10 10:47:06 +0200

    Modification du footer

--- includes/footer.php
+++ includes/footer.php
@@ -1,11 +1,6 @@
 <footer class="site-footer">
     <div class="footer-content">
         <p>&copy; <?php echo date("d/m/Y"); ?> Groupe 7</p>
-        <ul class="footer-links">
-            <li><a href="index.php">Accueil</a></li>
-            <li><a href="a-propos.php">À propos</a></li>
-            <li><a href="contact.php">Contact</a></li>
             <p>© 2026 SAE_2.456 Project</p>
-        </ul>
     </div>
 </footer>
