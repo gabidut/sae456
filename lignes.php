41560af136cef67da275dff80297d72042dbde07
Your Name 2 <you@example.com>
2026-06-10 10:01:34 +0200

    feat: Ajout du fichier lignes.php pour la visulaisation de lignes US-01

---
+++ lignes.php
@@ -0,0 +1,21 @@
+<?php
+
+require_once("config.php");
+
+?>
+
+
+
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Lignes</title>
+</head>
+<body>
+    <div id="lig-button-container">
+
+    </div>
+</body>
+</html>
