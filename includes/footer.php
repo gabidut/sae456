eaf4c7b068f8c793ff80556ee1a52dd9e9a54cd0
Armand <Armand>
2026-06-10 10:44:28 +0200

    implantation du css de footer

--- includes/footer.php
+++ includes/footer.php
@@ -1,7 +1,7 @@
-<footer>
-    <div>
+<footer class="site-footer">
+    <div class="footer-content">
         <p>&copy; <?php echo date("d/m/Y"); ?> Groupe 7</p>
-        <ul>
+        <ul class="footer-links">
             <li><a href="index.php">Accueil</a></li>
             <li><a href="a-propos.php">À propos</a></li>
             <li><a href="contact.php">Contact</a></li>
