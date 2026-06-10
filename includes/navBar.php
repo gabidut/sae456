4ec0d585189dddc9abd65f0c1a48792736f983dd
elias <elias>
2026-06-10 16:38:01 +0200

    fix titre cliquable

--- includes/navBar.php
+++ includes/navBar.php
@@ -6,13 +6,13 @@ $current_page = basename($_SERVER['PHP_SELF']);
     <div class="header-container">
         
         <div class="header-left">
-            <a href="index.php">
+            <a href="../index.php">
                 <img src="/image/car_vikingTransport.png" alt="Logo Viking Transport" class="header-logo-img">
             </a>
         </div>
 
         <div class="header-center">
-            <a href="index.php">
+            <a href="../index.php">
             <h1 class="main-title">Viking<span>Transport</span></h1>
             </a>
         </div>
