ea9a2d13a9a567956cdee3c34d9e3312a949761a
Your Name 2 <you@example.com>
2026-06-10 15:55:13 +0200

    File fortmat

--- modules/ligne.php
+++ modules/ligne.php
@@ -14,7 +14,7 @@ class Ligne
 
     public function getLignes()
     {
-        $sql = $sql = 
+        $sql = $sql =
             "SELECT DISTINCT REGEXP_REPLACE(LIG_NUM, '[^0-9]', '') AS LIG_NUM
             FROM VIK_LIGNE
             ORDER BY TO_NUMBER(REGEXP_REPLACE(LIG_NUM, '[^0-9]', '')) ASC";
@@ -27,16 +27,16 @@ class Ligne
     }
 
     // Ajoute cette fonction dans ta classe Ligne, juste en dessous de getLignes()
-    public function getDirections($numeroDeLigne) {
+    public function getDirections($numeroDeLigne)
+    {
         $sql = "SELECT LIG_NUM 
                 FROM VIK_LIGNE 
                 WHERE REGEXP_REPLACE(LIG_NUM, '[^0-9]', '') = :numero
                 ORDER BY LIG_NUM ASC";
-                
+
         $stmt = $this->database->prepareStatement($sql);
         $stmt->execute(['numero' => $numeroDeLigne]);
-        
+
         return $stmt->fetchAll(PDO::FETCH_COLUMN);
     }
-
 }
