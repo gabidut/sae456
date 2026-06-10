cae58c4c66e144f81d5ed7e8a9112100524ccd71
elias <elias>
2026-06-10 13:56:54 +0200

    bug fix

--- includes/global.php
+++ includes/global.php
@@ -11,7 +11,7 @@ include_once 'navBar.php'; // On inclut ton nouveau fichier ici
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Viking Transport</title>
-    <link rel="stylesheet" href="/assets/style/global.css">
+    <link rel="stylesheet" href="assets/style/global.css">
 </head>
 <body>
 
