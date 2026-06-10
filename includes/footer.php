a5354f1c4b4723992e4b7a6e02f9f515195063c9
Armand <Armand>
2026-06-10 11:43:53 +0200

    footer

--- includes/footer.php
+++ includes/footer.php
@@ -1,19 +1,9 @@
 <footer class="site-footer">
-    <div class="footer-container">
-        
-        <div class="footer-brand">
-            <img src="image/car_vikingTransport.png" alt="Les Casseurs Codeurs" class="footer-logo">
-        </div>
-
-        <div class="footer-nav">
-            <div class="footer-col">
+    <div class="footer-bottom">
+        <img src="image/logo_noir.png" alt="Les Casseurs Codeurs" class="footer-logo">
+        <p>&copy; <?php echo date("d/m/Y"); ?> Les Casseurs Codeurs. Tous droits réservés.</p>
+        <div class="footer-col">
                 <h3><a href="a_propos.php">À propos</a></h3>
             </div>
-        </div>
-
-    </div>
-
-    <div class="footer-bottom">
-        <p>&copy; <?php echo date("d/m/Y"); ?> Les Casseurs Codeurs. Tous droits réservés.</p>
     </div>
 </footer>
