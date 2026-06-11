20b44940b03fb288e89ff8ca28951621e7f3bb21
Victor <angerre251@unicaen.fr>
2026-06-11 15:29:43 +0200

    méthode getNbClientConnecteEntre

--- modules/admin.php
+++ modules/admin.php
@@ -18,12 +18,18 @@ class Adminitration
         $this->password_secret = $password_secret;
         $this->session_helper = $session_helper;
     }
-    
 
+    public function getNbClientConnecteEntre($datedebut, $datefin): array
+    {
+        $sql = "SELECT count(*) AS total_clients
+            FROM vik_client
+            WHERE TRUNC(cli_date_connec) BETWEEN TO_DATE(:datedebut, 'DD/MM/YY') AND TO_DATE(:datefin, 'DD/MM/YY')";
 
+        $stmt = $this->database->prepareStatement($sql);
+        $stmt->execute(['datedebut' => $datedebut, 'datefin' => $datefin]);
 
+        $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
-
-
-
+        return $result;
+    }
 }
