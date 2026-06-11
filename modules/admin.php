8bfa9e3a8af3fb7df6ff7e09657266554babc4f1
Ernault Kévin <kevin.ernault@etu.unicaen.fr>
2026-06-11 15:58:04 +0200

    fix list with like

--- modules/admin.php
+++ modules/admin.php
@@ -26,7 +26,7 @@ class Adminitration
         $stmt->execute(['cliNum' => $cliNum]);
     }
 
-    public function listClientsNum():array
+    public function listClients():array
     {
         $sql = "SELECT * FROM vik_client order by cli_num asc";
         $stmt = $this->database->prepareStatement($sql);
@@ -76,7 +76,7 @@ class Adminitration
 
     public function listClientsSortID($cliNum):array
     {
-        $sql = "SELECT * FROM vik_client where cli_num = :cliNum";
+        $sql = "SELECT * FROM vik_client where cli_num LIKE '%:cliNum%";
         $stmt = $this->database->prepareStatement($sql);
         $stmt->execute(['cliNum' => $cliNum]);
         return $stmt->fetchAll(PDO::FETCH_ASSOC);
@@ -84,7 +84,7 @@ class Adminitration
 
     public function listClientsSortCourriel($cli_courriel):array
     {
-        $sql = "SELECT * FROM vik_client where cli_courriel = :cli_courriel";
+        $sql = "SELECT * FROM vik_client where cli_courriel like '%:cli_courriel%'";
         $stmt = $this->database->prepareStatement($sql);
         $stmt->execute(['cli_courriel' => $cli_courriel]);
         return $stmt->fetchAll(PDO::FETCH_ASSOC);
@@ -92,7 +92,7 @@ class Adminitration
 
     public function listClientsSortRang($typ_num):array
     {
-        $sql = "SELECT * FROM vik_client where typ_num = :typ_num";
+        $sql = "SELECT * FROM vik_client where typ_num like '%:typ_num%'";
         $stmt = $this->database->prepareStatement($sql);
         $stmt->execute(['typ_num' => $typ_num]);
         return $stmt->fetchAll(PDO::FETCH_ASSOC);
@@ -100,7 +100,7 @@ class Adminitration
 
     public function listClientsSortPrenom($cli_prenom):array
     {
-        $sql = "SELECT * FROM vik_client where cli_prenom = :cli_prenom";
+        $sql = "SELECT * FROM vik_client where cli_prenom like '%:cli_prenom%'";
         $stmt = $this->database->prepareStatement($sql);
         $stmt->execute(['cli_prenom' => $cli_prenom]);
         return $stmt->fetchAll(PDO::FETCH_ASSOC);
@@ -108,7 +108,7 @@ class Adminitration
 
     public function listClientsSortNom($cli_nom):array
     {
-        $sql = "SELECT * FROM vik_client where cli_nom = :cli_nom";
+        $sql = "SELECT * FROM vik_client where cli_nom like '%:cli_nom%'";
         $stmt = $this->database->prepareStatement($sql);
         $stmt->execute(['cli_nom' => $cli_nom]);
         return $stmt->fetchAll(PDO::FETCH_ASSOC);
@@ -116,7 +116,7 @@ class Adminitration
 
     public function listClientsSortVille($cli_ville):array
     {
-        $sql = "SELECT * FROM vik_client where cli_ville = :cli_ville";
+        $sql = "SELECT * FROM vik_client where cli_ville like '%:cli_ville%'";
         $stmt = $this->database->prepareStatement($sql);
         $stmt->execute(['cli_ville' => $cli_ville]);
         return $stmt->fetchAll(PDO::FETCH_ASSOC);
