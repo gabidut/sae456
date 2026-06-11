e2c21ad28e0848d59adc3fff4cd5e45f6d3d4faf
Your Name 2 <you@example.com>
2026-06-11 09:01:44 +0200

    feat: modification du mdp

--- profil.php
+++ profil.php
@@ -5,10 +5,14 @@
 
     /*if(!$session->isUserLoggedIn())
     {
-        header('Location: index.php');
+        header('Location: auth/login/index.php');
     }  */
     
     //$infoClient = $session->getClientInfoFromId($_SESSION['user'])
+
+    $reservation_cli = $authentificator->getReservation($cliTest);
+
+
     if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 
         if(isset($_POST['btn_modif_info'])) {
@@ -37,6 +41,26 @@
                 $messageSucces = "Les modifications ont bien été enregistrées";
             }
         }
+
+        if(isset($_POST['btn_modif_mdp']))
+        {
+            $actualPassword = trim($_POST['mdp_act']);
+            $newPassword = trim($_POST['mdp_nouv']);
+
+            if (!empty($actualPassword) && !empty($newPassword)) {
+
+                if($authentificator->verify_password($actualPassword, $user['CLI_MDP']))
+                {
+                    $authentificator->changePassword($cliTest ,$newPassword);
+                    // $authentificator->changePassword($_SESSION['user'] ,$newPassword);
+
+                    $messageSucces = "Les modifications ont bien été enregistrées";
+                }
+                else{
+                    $messageSucces = "Mot de passe incorecte";
+                }
+            }
+        }
     
 }
 
@@ -54,8 +78,16 @@ $infoClient = $session->getClientInfoFromId($cliTest);
 <body>
     <?php if($infoClient): ?>
         <div class="profile-card">
-            <h2>Mon Espace</h2>
-            <span class="badge-client">Statut : <?= htmlspecialchars($infoClient['TYP_NOM']) ?></span>
+            <h1>Bienvenue <?=  $session->getUserSession()['CLI_PRENOM'] ?> !</h1>
+
+            <!--Bouton a mettre en haut a gauche-->
+            <button onclick="location.href = '/auth/logout'">Déconnexion</button>
+
+            <div class="status-client-container">
+                <span class="status-client">Statut : <?= htmlspecialchars($infoClient['TYP_NOM']) ?></span>
+                <span class="point-tot-client">Statut : <?= htmlspecialchars($infoClient['CLI_NB_POINTS_TOT']) ?></span>
+                <span class="point-ec-client">Statut : <?= htmlspecialchars($infoClient['CLI_NB_POINTS_EC']) ?></span>
+            </div>
 
             <div class="info-card">
                 <form method="post" action="">
@@ -70,6 +102,9 @@ $infoClient = $session->getClientInfoFromId($cliTest);
 
                     <h3>Prénom :</h3>
                     <input type="text" name="prenom_client" value="<?= htmlspecialchars($infoClient['CLI_PRENOM'])?>">
+
+                    <h3>Ville :</h3>
+                    <input type="text" name="prenom_client" value="<?= htmlspecialchars($infoClient['CLI_VILLE'])?>">
 
                     <br><br>
                     <input type="submit" class="btn-submit" value="Enregistrer les modifications" name="btn_modif_info">
@@ -93,6 +128,48 @@ $infoClient = $session->getClientInfoFromId($cliTest);
                     <br><br>
                     <input type="submit" class="btn-submit" value="Enregistrer les modifications" name="btn_modif_contact">
                 </form>
+            </div>
+
+            <div class="mdp-card">
+                <form method="post" action="">
+                    <h2>Contacte</h2>
+
+                    <?php if (isset($messageSucces)): ?>
+                        <div class="msg-succes"><?= $messageSucces ?></div>
+                    <?php endif; ?>
+
+                    <h3>Mot de passe Actuel :</h3>
+                    <input type="password" name="mdp_act" value="">
+
+                    <h3>Nouveau Mot de passe :</h3>
+                    <input type="password" name="mdp_nouv" value="">
+
+                    <br><br>
+                    <input type="submit" class="btn-submit" value="Enregistrer les modifications" name="btn_modif_mdp">
+                </form>
+            </div>
+
+            <div class="table-resa">
+                    <?php if($reservation_cli): ?>
+
+                        <table style="border-collapse: collapse; cellpadding: 10px;">
+                            <thead>
+                                <tr>
+                                    <th></th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <?php foreach($reservation_cli as $resa): ?>
+                                    <tr>
+
+                                    </tr>
+                                <?php endforeach;?>
+                            </tbody>
+                        </table>
+                        
+                    <?php else: ?>
+                            <p>Aucune réservation.</p>
+                    <?php endif; ?>
             </div>
         </div>
     <?php else: ?>
