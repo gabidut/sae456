0f9dc993499b6614357ec67e7c6732040408217c
couty251 <couty251@unicaen.fr>
2026-06-11 13:19:41 +0200

    fix - test

--- index.php
+++ index.php
@@ -5,7 +5,7 @@ include 'includes/global.php';
 
 <section class="hero">
     <h2>La puissance du transport <span>Nordique</span></h2>
-    <p>Logistique lourde, fret routier et solutions de transport express. Nous déplaçons vos marchandises avec la force et la rigueur des Vikings.</p>
+    <p>L'entreprise nordique qui vous emmenera là où vous avez besoin d'aller. Nous vous déplacons avec la force et la rigueur des Vikings.</p>
     <a href="#services" class="btn">Découvrir nos services</a>
 </section>
 
@@ -14,18 +14,18 @@ include 'includes/global.php';
     
     <div class="services-grid">
         <div class="service-card">
-            <h4>Lorem ipsum dolor sit amet</h4>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime adipisci maiores architecto eius beatae sunt odit exercitationem ducimus earum consequuntur error natus quam enim sequi provident cumque, qui, ut iusto!</p>
+            <h4>TRAJETS OPTIMISÉS</h4>
+            <p>Ne perdez plus une minute. Notre système intelligent calcule instantanément le trajet le plus rapide en temps réel. Moins d'attente, moins de bouchons, et une ponctualité garantie à chaque voyage !</p>
         </div>
+    
+        <div class="service-card">
+                <h4>TRANSPORT NORMAND</h4>
+                <p>Que ce soit pour vos déplacements professionnels, personnels ou touristiques, nous vous transportons partout en Normandie. Profitez d'un trajet confortable allant du Mont-Saint-Michel jusqu'à Rouen !</p>
+            </div>
         
         <div class="service-card">
-            <h4>Lorem ipsum dolor sit amet</h4>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime adipisci maiores architecto eius beatae sunt odit exercitationem ducimus earum consequuntur error natus quam enim sequi provident cumque, qui, ut iusto!</p>
-        </div>
-        
-        <div class="service-card">
-            <h4>Lorem ipsum dolor sit amet</h4>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime adipisci maiores architecto eius beatae sunt odit exercitationem ducimus earum consequuntur error natus quam enim sequi provident cumque, qui, ut iusto!</p>
+            <h4>PROGRAMME DE FIDÉLITÉ</h4>
+            <p>Votre fidélité mérite d'être récompensée. Cumulez des points à chaque trajet effectué et profitez de réductions exclusives, de voyages gratuits et d'avantages réservés à nos membres inscrits !</p>
         </div>
     </div>
 </section>
