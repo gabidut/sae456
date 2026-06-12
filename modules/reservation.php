68441426e8eb28f5225e2454ba323cbf3bc106db
Ernault Kévin <kevin.ernault@etu.unicaen.fr>
2026-06-12 09:27:26 +0200

    gestion des points SQL

--- modules/reservation.php
+++ modules/reservation.php
@@ -337,4 +337,47 @@ class Reservation
 
         return ['prix' => number_format($prix, 2, '.', '')];
     }
+
+    public function usePoints($cliNum, $pointsToUse):int
+    {
+        $sql = "UPDATE vik_client SET CLI_nb_POINTS_ec = CLI_nb_POINTS_ec - :pointsToUse WHERE CLI_NUM = :cliNum";
+        $stmt = $this->database->prepareStatement($sql);
+        $stmt->execute(['pointsToUse' => $pointsToUse, 'cliNum' => $cliNum]);
+        $reduc = 0;
+        if($pointsToUse == 100) {
+            $reduc = 1;
+        } else if ($pointsToUse == 500) {
+            $reduc = 7;
+        }else if ($pointsToUse == 1000) {
+            $reduc = 15;
+        }
+        return $reduc;
+    }
+
+    public function usablePoints100($cliNum):bool
+    {
+        $sql = "SELECT CLI_nb_POINTS_ec FROM vik_client WHERE CLI_NUM = :cliNum";
+        $stmt = $this->database->prepareStatement($sql);
+        $stmt->execute(['cliNum' => $cliNum]);
+        $result = $stmt->fetch(PDO::FETCH_ASSOC);
+        return $result && (int) $result['CLI_nb_POINTS_ec'] >= 100;
+    }
+
+    public function usablePoints500($cliNum):bool
+    {
+        $sql = "SELECT CLI_nb_POINTS_ec FROM vik_client WHERE CLI_NUM = :cliNum";
+        $stmt = $this->database->prepareStatement($sql);
+        $stmt->execute(['cliNum' => $cliNum]);
+        $result = $stmt->fetch(PDO::FETCH_ASSOC);
+        return $result && (int) $result['CLI_nb_POINTS_ec'] >= 500;
+    }
+
+    public function usablePoints1000($cliNum):bool
+    {
+        $sql = "SELECT CLI_nb_POINTS_ec FROM vik_client WHERE CLI_NUM = :cliNum";
+        $stmt = $this->database->prepareStatement($sql);
+        $stmt->execute(['cliNum' => $cliNum]);
+        $result = $stmt->fetch(PDO::FETCH_ASSOC);
+        return $result && (int) $result['CLI_nb_POINTS_ec'] >= 1000;
+    }
 }
